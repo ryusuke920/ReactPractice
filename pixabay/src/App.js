e67b4ab4978439e3c47d4c872c16f745fbf5ab91
ryusuke <ryusuke_920@icloud.com>
2021-11-12 21:27:04 +0900

    add react-app

---
+++ pixabay/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+const App = () => {
+    const onSearchSubmit = (term) => {
+        console.log(term);
+    };
+
+    return (
+        <div className='ui container' style={{marginTop: '20px'}}>
+            <SearchBar onSubmit={onSearchSubmit}/>
+        </div>
+    );
+};
+
+export default App;
